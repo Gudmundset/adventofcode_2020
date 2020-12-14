00ff640faf24d58e9ad5ac085a5999c606d60314
Philip A Larson <dynendal@gmail.com>
2020-12-14 00:04:26 -0800

    Update day14.py
    Part 2 added

--- day14.py
+++ day14.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python
 
 import re
+from itertools import combinations
+
+
+def gen_mask_multiplicity(mask_combo, address, length):
+    new_addressdict = {}
+    address = list(address)
+    for combo in mask_combo.keys():
+        new_addressdict[combo] = (0,1)
+    for add,com in new_addressdict.items():
+        for c in com:
+            address[add] = str(c)
+            if not 'X' in address:
+                yield ''.join(address)
 
 def part_1_2():
     """docking rules"""
     mem = {}
+    mem_part2 = {}
     mem_re = r'mem\[(?P<mem>\d+)\] = (?P<num>\d+)'
     maskdict = {}
     memlist = []
@@ -24,25 +38,48 @@ def part_1_2():
 
         for mask,overwrites in maskdict.items():
             maskoverwrite = {n:o for n,o in reversed(list(enumerate(mask))) if o == '1' or o == '0'}
-            print(maskoverwrite)
+            part2_maskoverwrite = {n:o for n,o in reversed(list(enumerate(mask))) if o != '0'}
             for mempair in overwrites:
                 address = mempair[0]
                 num = format(int(mempair[1], 10), '036b')
-                print(len(num),int(address))
-                print("value:\t", num)
-                print("address:\t", num)
-                print("mask:\t", mask)
+                address_num = format(int(address, 10), '036b')
+                print('--')
+                print("part1\nvalue:\t\t", num)
+                print("mask:\t\t", mask)
                 newnum = ''
                 for n,a in enumerate(num):
                     if n in maskoverwrite:
                         newnum += maskoverwrite[n]
                     else:
                         newnum += a
-                print("result:\t", newnum)
+                newaddress = ''
+                for n2,a2 in enumerate(address_num):
+                    if n2 in part2_maskoverwrite:
+                        newaddress += part2_maskoverwrite[n2]
+                    else:
+                        newaddress += a2
+                print("result:\t\t", newnum)
+                print("part2\naddress:\t", address_num)
+                print("mask:\t\t", mask)
+                print("result:\t\t", newaddress)
+                print(part2_maskoverwrite)
                 mem[address] = newnum
+
+                part2_maskcombo = {n:o for n,o in reversed(list(enumerate(mask))) if o == 'X'}
+                newadds = []
+                for x in range(len(part2_maskcombo.keys())):
+                    for gen in gen_mask_multiplicity(part2_maskcombo, newaddress, len(part2_maskcombo.keys())):
+                        newadds.append(gen)
+                print(newadds)
+                for add in newadds:
+                    mem_part2[add] = num
         
         testlist = []
         for k,v in mem.items():
+            testlist.append(int(v.lstrip('0'), 2))
+        print(sum(testlist))
+        testlist = []
+        for k,v in mem_part2.items():
             testlist.append(int(v.lstrip('0'), 2))
         print(sum(testlist))
                 
@@ -50,3 +87,4 @@ def part_1_2():
                     
 if __name__ == "__main__":
     part_1_2()
+    
