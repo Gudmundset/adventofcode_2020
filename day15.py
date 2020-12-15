91f77ea40228926741da167bca999feeafeb6f90
Philip A Larson <dynendal@gmail.com>
2020-12-14 22:20:21 -0800

    Create day15.py

---
+++ day15.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+from collections import Counter
+
+def part_1_2():
+    """memory game"""
+    mem_dict = Counter()
+    # example_input = [0,3,6]
+    # example_input = [1,3,2]
+    # example_input = [2,1,3]
+    # example_input = [1,2,3]
+    # example_input = [2,3,1]
+    # example_input = [3,2,1]
+    # example_input = [3,1,2]
+    final_input = [1,12,0,20,8,16]
+
+    the_list = final_input
+    mem_dict = {a:0 for a in the_list}
+    memory = []
+    firstseen = False
+
+    for i in range(0,2020):
+        if i < len(the_list):
+            turn = the_list[i]
+        else: 
+            if not firstseen:
+                turn = 0
+                mem_dict[turn] = i
+                memory.append(turn)
+                print(f"Turn {i+1}: ", turn)
+                firstseen = True
+                continue
+            else:
+                turn = memory[-1]
+                for num, appearance in enumerate(reversed(list(memory))):
+                    if num == 0:
+                        continue
+                    if turn == appearance:
+                        turn = num
+                        break
+                if not turn in mem_dict:
+                    firstseen = False
+                mem_dict[turn] = i
+        print(f"Turn {i+1}: ", turn)
+        memory.append(turn)
+        
+          
+if __name__ == "__main__":
+    part_1_2()
+    
